c501dee85362392e83cef4a4333640950a1e0b0c
artidoro <apagnoni@college. harvard.edu>
2016-04-24 19:07:16 -0400

    cleanup

--- reading.py
+++
@@ -1,7 +0,0 @@
-infile = open('input.txt')
-soln = []
-for line in infile:
-	soln.append(line)
-soln = [int(i) for i in soln]
-print soln
-infile.close()	
--- write.py
+++
@@ -1,5 +0,0 @@
-file = open('output.txt','w')
-for i in range(99):
-	file.write(str(i)+'\n')
-file.write('99')
-file.close()
